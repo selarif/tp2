a1029fe7ed58c2fe7561b0732439fa3257e05d88
Sami El Arif <75094123+selarif@users.noreply.github.com>
2021-04-23 13:23:52 -0400

    Premier commit de l'exerice 4

--- template-parts/content-galerie.php
+++ template-parts/content-galerie.php
@@ -13,7 +13,7 @@
     <div class="flip-card-inner">
         <div class="flip-cart-front">
             <?php the_post_thumbnail( 'thumbnail' ); ?>
-        </div>
+        </div> 
 
         <div class="flip-card-back"> <!-- Sera affiche en arriere. pcq les deux (front et back) sont en absolutes -->
             <h6><a href="<?php echo get_permalink(); ?>"><?php the_title() ?></a></h6>
